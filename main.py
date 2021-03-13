4184161337954d7df700fcbb25c47ff5b3afd978
Fran Vallejo <neotherack@gmail.com>
2021-03-13 01:24:54 +0100

    sql handler bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from website import Website
-from sql import connect, create_table, insert_record
+from sql import connect, create_table, insert_record, commit
 
 config = ["elmundo.es","infolibre.es","laopinioncoruna.es",\
   "diariodesevilla.es","abc.es","lavanguardia.com","laverdad.es",\
@@ -15,3 +15,8 @@ if conn:
         print(f"{site.url} processed")
         #print(site.clear_text[site.OFFSET:5000+site.OFFSET])
         insert_record(conn, site.url, site.noisy_text, site.clear_text, str(site.encoded_data))
+
+    commit(conn)
+    print("DB commit, you're done!")
+else:
+    print("Error on db access!")
--- sql.py
+++ sql.py
@@ -2,14 +2,14 @@ import sqlite3
 
 def connect():
     try:
-        return sqlite3.connect('websites.db')
+        return sqlite3.connect('data_websites.db')
     except Exception as e:
         print(f"Connection failed\n{e}")
         return None
 
 def create_table(c):
     try:
-        sql="create table daily_news (url text not null, noisy_text text, clear_text text, encoded_values text, date_time datetime default current_date)"
+        sql="create table daily_news (url text not null, noisy_text text, clear_text text, encoded_values text, created_at datetime default current_datetime)"
         c.execute(sql)
     except Exception as e:
         print(f"{e}")
